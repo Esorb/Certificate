0263619a5d5dd725f8f64c61a378ed21374f42a8
Esorb <116953670+Esorb@users.noreply.github.com>
2023-04-02 13:30:51 +0200

    added memory for selected page in MainWindow

--- Esorb.Certificate/Esorb.Certificate.App/MainWindow.xaml.cs
+++ Esorb.Certificate/Esorb.Certificate.App/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ public partial class MainWindow : Window
     private ICertificateSettingsViewModel SettingsVM;
     private IList<NavButton> navButtons = new List<NavButton>();
     private IDictionary<Uri, Page> pages = new Dictionary<Uri, Page>();
+
     public MainWindow()
     {
         InitializeComponent();
@@ -31,12 +32,138 @@ public partial class MainWindow : Window
         SetApplicationStatus();
     }
 
+
     private void SetApplicationStatus()
     {
         var btn = GetLastActivatedNavButton();
         btn.Selected = true;
         AppFrame.Navigate(pages[btn.NavUri!]);
     }
+
+
+    private GridLength GetLastMenuWidth()
+    {
+        GridLength result;
+
+        switch (certifcateViewModel.CertificateSettingsViewModel.MenuPosition)
+        {
+            case "wide":
+                return new GridLength(110, GridUnitType.Pixel);
+            default:
+                return new GridLength(44, GridUnitType.Pixel);
+        }
+    }
+
+
+    private void InitPages()
+    {
+        pages.Add(BtnStart.NavUri!, new StartPage(certifcateViewModel));
+        pages.Add(BtnInput.NavUri!, new InputPage(certifcateViewModel));
+        pages.Add(BtnExport.NavUri!, new ExportPage(certifcateViewModel));
+        pages.Add(BtnAdmin.NavUri!, new AdminPage(certifcateViewModel));
+        pages.Add(BtnInfo.NavUri!, new InfoPage(certifcateViewModel));
+    }
+
+
+    private void InitMenu()
+    {
+
+    }
+
+
+    private void AddNavButtons()
+    {
+        navButtons.Add(BtnMenue);
+        navButtons.Add(BtnStart);
+        navButtons.Add(BtnInput);
+        navButtons.Add(BtnExport);
+        navButtons.Add(BtnAdmin);
+        navButtons.Add(BtnInfo);
+    }
+
+
+    private void CloseButton_Click(object sender, RoutedEventArgs e)
+    {
+        this.Close();
+    }
+
+
+    private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+    {
+        this.WindowState = WindowState.Minimized;
+    }
+
+
+    private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (this.WindowState == WindowState.Maximized)
+        {
+            var width = this.ActualWidth;
+            var height = this.ActualHeight;
+            this.Width = width - 2;
+            this.Height = height - 2;
+            this.Top = 1;
+            this.Left = 1;
+
+            this.WindowState = WindowState.Normal;
+        }
+        this.DragMove();
+    }
+
+
+    private void Border_MouseUp(object sender, MouseButtonEventArgs e)
+    {
+        if (this.WindowState == WindowState.Normal)
+        {
+            this.WindowState = WindowState.Maximized;
+        }
+    }
+
+
+    private void BtnMenue_Click(object sender, RoutedEventArgs e)
+    {
+        GridLength smallGridLength = new GridLength(44, GridUnitType.Pixel);
+        GridLength bigGridLength = new GridLength(110, GridUnitType.Pixel);
+        if (NavColumn.Width.Equals(bigGridLength))
+        {
+            NavColumn.Width = smallGridLength;
+            SettingsVM.MenuPosition = "narrow";
+        }
+        else
+        {
+            NavColumn.Width = bigGridLength;
+            SettingsVM.MenuPosition = "wide";
+        }
+    }
+
+
+    private void StackPanel_Click(object sender, RoutedEventArgs e)
+    {
+
+        var ClickedNavButton = e.OriginalSource as NavButton;
+        if (ClickedNavButton.NavUri != null)
+        {
+            foreach (var navBtn in navButtons)
+            {
+                navBtn.Selected = false;
+            }
+            AppFrame.Navigate(pages[ClickedNavButton.NavUri]);
+            ClickedNavButton.Selected = true;
+            SetLastActivatedNavButton(ClickedNavButton);
+        }
+    }
+
+
+    private void SetLastActivatedNavButton(NavButton btn)
+    {
+        if (btn == BtnStart) { SettingsVM.Page = "Start"; }
+        else if (btn == BtnInput) { SettingsVM.Page = "Input"; }
+        else if (btn == BtnExport) { SettingsVM.Page = "Export"; }
+        else if (btn == BtnAdmin) { SettingsVM.Page = "Admin"; }
+        else if (btn == BtnInfo) { SettingsVM.Page = "Info"; }
+        else { SettingsVM.Page = "Start"; }
+    }
+
 
     private NavButton GetLastActivatedNavButton()
     {
@@ -57,111 +184,5 @@ public partial class MainWindow : Window
             default:
                 return BtnStart;
         }
-    }
-
-    private GridLength GetLastMenuWidth()
-    {
-        GridLength result;
-
-        switch (certifcateViewModel.CertificateSettingsViewModel.MenuPosition)
-        {
-            case "wide":
-                return new GridLength(110, GridUnitType.Pixel);
-            default:
-                return new GridLength(44, GridUnitType.Pixel);
-        }
-
-        return result;
-    }
-
-    private void InitPages()
-    {
-        pages.Add(BtnStart.NavUri!, new StartPage(certifcateViewModel));
-        pages.Add(BtnInput.NavUri!, new InputPage(certifcateViewModel));
-        pages.Add(BtnExport.NavUri!, new ExportPage(certifcateViewModel));
-        pages.Add(BtnAdmin.NavUri!, new AdminPage(certifcateViewModel));
-        pages.Add(BtnInfo.NavUri!, new InfoPage(certifcateViewModel));
-    }
-
-    private void InitMenu()
-    {
-
-    }
-
-    private void AddNavButtons()
-    {
-        navButtons.Add(BtnMenue);
-        navButtons.Add(BtnStart);
-        navButtons.Add(BtnInput);
-        navButtons.Add(BtnExport);
-        navButtons.Add(BtnAdmin);
-        navButtons.Add(BtnInfo);
-    }
-
-    private void CloseButton_Click(object sender, RoutedEventArgs e)
-    {
-        this.Close();
-    }
-
-    private void MinimizeButton_Click(object sender, RoutedEventArgs e)
-    {
-        this.WindowState = WindowState.Minimized;
-    }
-
-    private void Border_MouseDown(object sender, MouseButtonEventArgs e)
-    {
-        if (this.WindowState == WindowState.Maximized)
-        {
-            var width = this.ActualWidth;
-            var height = this.ActualHeight;
-            this.Width = width - 2;
-            this.Height = height - 2;
-            this.Top = 1;
-            this.Left = 1;
-
-            this.WindowState = WindowState.Normal;
-        }
-        this.DragMove();
-    }
-
-    private void Border_MouseUp(object sender, MouseButtonEventArgs e)
-    {
-        if (this.WindowState == WindowState.Normal)
-        {
-            this.WindowState = WindowState.Maximized;
-        }
-    }
-
-    private void BtnMenue_Click(object sender, RoutedEventArgs e)
-    {
-        GridLength smallGridLength = new GridLength(44, GridUnitType.Pixel);
-        GridLength bigGridLength = new GridLength(110, GridUnitType.Pixel);
-        if (NavColumn.Width.Equals(bigGridLength))
-        {
-            NavColumn.Width = smallGridLength;
-            SettingsVM.MenuPosition = "narrow";
-        }
-        else
-        {
-            NavColumn.Width = bigGridLength;
-            SettingsVM.MenuPosition = "wide";
-        }
-    }
-
-    private void StackPanel_Click(object sender, RoutedEventArgs e)
-    {
-
-        var ClickedNavButton = e.OriginalSource as NavButton;
-        if (ClickedNavButton.NavUri != null)
-        {
-            foreach (var navBtn in navButtons)
-            {
-                navBtn.Selected = false;
-            }
-            AppFrame.Navigate(pages[ClickedNavButton.NavUri]);
-            ClickedNavButton.Selected = true;
-        }
-
-
     }
 }
